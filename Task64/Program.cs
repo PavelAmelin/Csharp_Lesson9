f24232985782183b57779b7a995486cef015c005
Pavel <sivec_p@mail.ru>
2023-02-11 17:54:46 +0300

    Add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,11 @@
+﻿void AllNums(int N)
+{
+    if (N > 0)
+    {
+    Console.WriteLine(N);
+    AllNums(N - 1);
+    }
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+AllNums(num);
