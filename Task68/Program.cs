17353b1999acf189e710304b54bd4c728793e9a7
Pavel <sivec_p@mail.ru>
2023-02-11 17:57:03 +0300

    Add Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,12 @@
+﻿int AkkermanFunc(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return AkkermanFunc(m - 1, 1);
+    return  AkkermanFunc(m - 1, AkkermanFunc(m , n - 1));
+}
+Console.WriteLine("Введите 2 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(AkkermanFunc(a, b));
