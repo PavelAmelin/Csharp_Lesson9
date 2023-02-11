5aa39717b36eb149fccbc785816fb690c331a1e0
Pavel <sivec_p@mail.ru>
2023-02-11 17:56:37 +0300

    Add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿int SumAllNumsBetween(int N, int M)
+{
+    if (N == 0 && M == 0)
+        return 0;
+    if (N == M)
+        return N;
+    else if (N > M)
+        return  M + SumAllNumsBetween(N, M + 1);
+    return  N + SumAllNumsBetween(N + 1, M);
+}
+Console.WriteLine("Введите 2 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел между {a} и {b}: {SumAllNumsBetween(a, b)}");
